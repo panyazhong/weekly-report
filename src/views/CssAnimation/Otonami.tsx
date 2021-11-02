44ef7ed75f8f778c27be0419c1efc2cd68048c9b
panyazhong <pyz1226149198@gmail.com>
2021-11-02 18:19:25 +0800

    add electron

--- src/views/CssAnimation/Otonami.tsx
+++ src/views/CssAnimation/Otonami.tsx
@@ -1,18 +1,17 @@
 const Otonami = () => {
   return (
-    <div className={'otonami-box'}>
+    <div className={"otonami-box"}>
       {/* <div className={'otonami-item otonami-1'}></div>
       <div className={'otonami-item otonami-2'}></div> */}
-      <div className={'otonami-item otonami-3'}></div>
-      <div className={'otonami-item otonami-4'}></div>
-      <div className={'otonami-item otonami-5'}></div>
-      <div className={'otonami-item otonami-6'}></div>
-      <div className={'otonami-item otonami-7'}></div>
-      <div className={'otonami-item otonami-8'}></div>
-      <div className={'otonami-item otonami-10'}></div>
-      <div className={'otonami-item otonami-11'}></div>
-      <div className={'otonami-item otonami-12'}></div>
-      <div className={'otonami-item otonami-13'}></div>
+      <div className={"otonami-item otonami-3"}></div>
+      <div className={"otonami-item otonami-4"}></div>
+      <div className={"otonami-item otonami-5"}></div>
+      <div className={"otonami-item otonami-6"}></div>
+      <div className={"otonami-item otonami-7"}></div>
+      <div className={"otonami-item otonami-8"}></div>
+      <div className={"otonami-item otonami-10"}></div>
+      <div className={"otonami-item otonami-11"}></div>
+      <div className={"otonami-item otonami-12"}></div>
       {/* <div className={'otonami-item otonami-14'}></div>
       <div className={'otonami-item otonami-15'}></div> */}
     </div>
